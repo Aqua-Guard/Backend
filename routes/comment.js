1bc26443f46a020a81227541c39fd539c7ad245b
Kout Mohamed <115358719+Mohamedkout15@users.noreply.github.com>
2023-12-22 02:52:47 +0100

    update

--- routes/comment.js
+++ routes/comment.js
@@ -1,28 +1,109 @@
-import express from 'express';
-import { body } from 'express-validator';
-import BadWordsFilter from 'bad-words';
-import { deleteComment, updateComment } from '../controllers/comment.js';
+import Comment from '../models/comment.js';
+import { validationResult } from "express-validator";
 
 
-const router = express.Router();
-const filter = new BadWordsFilter();
+// Add a comment to a post
+export const addComment = async (req, res) => {
+    const userId = req.user.userId;
+    console.log(req.params.postId, userId, req.body.content) // Current user
+    if (!validationResult(req).isEmpty()) {
+        return res.status(400).json({ errors: validationResult(req).array() });
+    } else {
+        const newComment = new Comment({
+            postId: req.params.postId,
+            userId: userId,
+            comment: req.body.comment,
+        });
 
-router
-    .route('/:commentId')
-    .put(
-        body('comment')
-            .notEmpty()
-            .trim()
-            .isLength({ min: 10, max: 100 })
-            .withMessage('The comment must be between 10 and 100 characters long.')
-            .custom((value) => {
-                if (filter.isProfane(value)) {
-                    throw new Error('The comment contains inappropriate language.');
-                }
-                return true;
-            })
-        , updateComment)
-    .delete(deleteComment);
+        try {
+            const savedComment = await newComment.save();
+            res.status(201).send({ message : "Comment added successfully"});     
+        } catch (error) {
+            res.status(500).send({ message : "Error adding comment"}); 
+        }
+    }
+};
+
+export const getCommentsByPost = async (req, res) => {
+    const postId = req.params.postId;
+    const userId = req.user.userId;
+    console.log(userId)
+    try {
+        const comments = await Comment.find({ postId: postId })
+            .populate('userId', 'firstName lastName image') // Populate only specific fields
+            .lean();
+        const formattedComments = comments.map(comment => {
+            return {
+                idUser: userId, 
+                idPost: postId, 
+                idComment: comment._id,
+                commentAvatar: comment.userId.image,
+                commentUsername: `${comment.userId.firstName} ${comment.userId.lastName}`,
+                comment: comment.comment
+            };
+        }).filter(comment => comment !== null); // Filter out null comments
+
+        res.json(formattedComments);
+
+    } catch (error) {
+        console.error('Error getting comments for post:', error);
+        res.status(500).send('Error getting comments');
+    }
+};
 
 
-export default router;
+export const getCommentsByIdPost = async (postId) => {
+    try {
+        const comments = await Comment.find({ postId: postId })
+            .populate('userId', 'userId firstName lastName image') // Populate only specific fields
+            .lean();
+        return comments.map(comment => {
+            return {
+                idUser: comment.userId._id,
+                idPost: comment.postId._id,
+                idComment : comment._id,
+                commentAvatar: comment.userId.image,
+                commentUsername: `${comment.userId.firstName} ${comment.userId.lastName}`,
+                comment: comment.comment
+            };
+        });
+            
+    } catch (error) {
+        console.error('Error getting comments for post:', error);
+        return []; // Return an empty array on error
+    }
+};
+
+
+// Update a specific comment
+export const updateComment = async (req, res) => {
+    const { commentId } = req.params;
+    if (!validationResult(req).isEmpty()) {
+        return res.status(400).json({ errors: validationResult(req).array() });
+    } else {
+        try {
+            const updatedComment = await Comment.findByIdAndUpdate(commentId, req.body, { new: true });
+            res.status(200).json(updatedComment);
+        } catch (error) {
+            res.status(500).json({ message: 'Error updating comment', error });
+        }
+    }
+};
+
+// Delete a specific comment
+export const deleteComment = async (req, res) => {
+    const { commentId } = req.params;
+    if (!commentId) {
+        return res.status(400).json({ error: "Comment ID is required." });
+    }
+    try {
+        const deletedComment = await Comment.findByIdAndDelete(commentId);
+        if (!deletedComment) {
+            return res.status(404).json({ message: 'Comment not found' });
+        }
+        res.status(200).json({ message: 'Comment deleted successfully' });
+    } catch (error) {
+        res.status(500).json({ message: 'Error deleting comment', error });
+    }
+};
+
